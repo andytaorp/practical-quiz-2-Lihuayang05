b421ba9cb37e74ef1f2b99b070286c0563f7eac9
cotton-l <huayang.l0013@gmail.com>
2024-11-28 15:45:13 +0800

    Commit

--- src/components/Habit.js
+++ src/components/Habit.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 export default function Habit({ habit, onToggle, onDelete }) {
+  const completedStyle = {
+    textDecoration: "line-through",
+  };
+
   return (
-    <li className={habit.completed ? "completed" : ""}>
-      <span>{habit.name}</span>
+    <li>
+      <span style={habit.completed ? completedStyle : {}}>{habit.name}</span>
       
       <button onClick={() => onToggle(habit.id)}>
         {habit.completed ? "Mark as Not Completed" : "Mark as Completed"}
